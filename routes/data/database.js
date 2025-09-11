6cbddce059b0bc2ae43d3f830a1a8551df327f7b
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-09-11 09:55:55 -0500

    Updated server and database file

---
+++ routes/data/database.js
@@ -0,0 +1,32 @@
+const env = require('dotenv').config();
+
+const { MongoClient } = require('mongodb');
+
+let database;
+
+const initDb = (callback) => {
+    if (database) {
+        console.log('Database is already initialized!');
+        return callback(null, database);
+    }
+    MongoClient.connect(process.env.MONGODB_URI)
+        .then((client) => {
+            database = client.db;
+            callback(null, database);
+        })
+        .catch((err) => {
+            callback(err);
+        });
+};
+
+const getDatabase = () => {
+    if (!database) {
+        throw new Error('Database not initialized');
+    }
+    return database;
+}
+
+module.exports = {
+    initDb,
+    getDatabase,
+};
--- server.js
+++ server.js
@@ -2,11 +2,19 @@ const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
 
+
+const port = process.env.PORT || 3000;
 const mongodb = require('./routes/data/database')
 
 app.use('/', require('./routes/contacts'));
 
-const port = process.env.PORT ||3000;
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => {
+            console.log(`Listening on port ${port}`);
+        });
+    }
 });
+
