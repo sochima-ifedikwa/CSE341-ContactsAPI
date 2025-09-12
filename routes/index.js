1460c1caa143d45e1c378ad6d3225f094a1faa00
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-09-11 21:03:40 -0500

    uncommented first get request

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-// router.get('/', (req, res) => {
-//     res.send('Hello World');
-// });
+router.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
 router.use('/contacts', require('./contacts'));
 module.exports = router;
