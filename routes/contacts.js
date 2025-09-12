3e34730e7cf5d241470a4c1f97c1f470ef174d32
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-09-11 20:47:14 -0500

    updated the contacts.js file to accept 2 get requests

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello World');
-});
+const contactsController = require('../controllers/contacts');
+
+router.get('/', contactsController.getAll);
+router.get('/:id', contactsController.getSingle); 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require('express');
-const dotenv = require('dotenv');
+// const dotenv = require('dotenv');
+require('dotenv').config();
 const app = express();
 
 
 const port = process.env.PORT || 3000;
-const mongodb = require('./routes/data/database')
+const mongodb = require('./data/database')
 
-app.use('/', require('./routes/contacts'));
+app.use('/', require('./routes/index'));
 
 mongodb.initDb((err) => {
     if (err) {
         console.log(err);
     } else {
         app.listen(port, () => {
-            console.log(`Listening on port ${port}`);
+            console.log(`Database is connected and listening on port ${port}`);
         });
     }
 });
-
