8152653cc3549efb32f1354d20377088d162faba
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-09-16 01:05:50 -0500

    Integrates Swagger UI to provide interactive API documentation for the Contacts API.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 
 router.get('/', (req, res) => {
     res.send('Hello World');
 });
 
 router.use('/contacts', require('./contacts'));
+        
+// Add the Swagger UI route
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 module.exports = router;
---
+++ swagger.js
@@ -0,0 +1,18 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'Contacts API',
+    description: 'API for managing contacts'
+  },
+  host: 'localhost:3000'
+};
+
+const outputFile = './swagger.json'; // Output file for the generated Swagger documentation should be created in the root directory EMPTY
+const routes = ['./routes/index.js', './routes/contacts.js'];
+
+/* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
+root file where the route starts, such as index.js, app.js, routes.js, etc ... */
+
+swaggerAutogen(outputFile, routes, doc);
+
