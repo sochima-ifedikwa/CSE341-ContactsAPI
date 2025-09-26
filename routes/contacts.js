72bc8bba6edd3c407fca06e5cf4ad36426b82387
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-09-26 16:30:54 -0500

    update files

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,13 +3,12 @@ const router = express.Router();
 
 
 const contactsController = require('../controllers/contacts');
-const validation = require('../middleware/validate.js');
 
 router.get('/', contactsController.getAll); // Get all contacts
 router.get('/:id', contactsController.getSingle); // Get a single contact by ID
 
-router.post('/', validation.saveContact, contactsController.createContact); // Create a new contact
-router.put('/:id', validation.saveContact, contactsController.updateContact); // Update an existing contact
+router.post('/', contactsController.createContact); // Create a new contact
+router.put('/:id', contactsController.updateContact); // Update an existing contact
 
 router.delete('/:id', contactsController.deleteContact); // Delete a contact
 
